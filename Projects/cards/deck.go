ee95735fa347fe40e1df5d99cb9047b4aa744edd
Andre Sampaio <andre.sampaio@fit-tecnologia.org.br>
2022-05-17 11:24:24 -0300

    add first test

--- Projects/cards/deck.go
+++ Projects/cards/deck.go
@@ -3,8 +3,10 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"math/rand"
 	"os"
 	"strings"
+	"time"
 )
 
 // Create a new type of 'deck'
@@ -53,6 +55,16 @@ func newDeckFromFile(filename string) deck {
 		os.Exit(1)
 	}
 
-	deck := strings.Split(string(file), ",")
-	return deck
+	s := strings.Split(string(file), ",")
+	return deck(s)
+}
+
+func (d deck) shuffle() {
+	source := rand.NewSource(time.Now().UnixNano())
+	r := rand.New(source)
+
+	for i := range d {
+		random := r.Intn(len(d) - 1)
+		d[i], d[random] = d[random], d[i]
+	}
 }
