a3049786f31b1416551fbf35e13bf37c67507337
apsampaio <andre03.sampaio@gmail.com>
2022-11-08 20:05:53 -0300

    ninja level 3

---
+++ course-2/03-ninja-level-3/exercises/exercise-03.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+/*
+
+Create a for loop using this syntax
+	 for condition { }
+Have it print out the years you have been alive.
+
+*/
+
+func main() {
+	for i := 1997; i <= 2022; i++ {
+		fmt.Println(i)
+	}
+}
